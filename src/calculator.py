6e1e40aad699d5a5bb3b7ab02abed7b7276c70fa
HUBRILITY <85511064+HUBRILITY@users.noreply.github.com>
2021-07-05 16:39:29 -0400

    Task: Successfully deployed container.
    Created image, container, interpreter

---
+++ src/calculator.py
@@ -0,0 +1,5 @@
+class calculator:
+    result = 0
+
+    def __init__(self):
+        pass
--- src/calculatorTests.py
+++ src/calculatorTests.py
@@ -1,10 +1,13 @@
 import unittest
+from calculator import calculator
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+
+    def test_instantiate_calculator(self):
+        calculator = calculator()
+        self.assertIsInstance(calculator,calculator)
 
 
-if __name__ == '__main__':
+if __name__ != '__main__':
     unittest.main()
