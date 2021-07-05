68c6868e36865d09ca6239ae215f18331fac4c8d
HUBRILITY <85511064+HUBRILITY@users.noreply.github.com>
2021-07-05 19:25:56 -0400

    Task: Removed redundant code with def setUP

--- src/CalculatorTests.py
+++ src/CalculatorTests.py
@@ -8,22 +8,18 @@ class MyTestCase(unittest.TestCase):
         self.calculator = self.calculator()
 
     def test_instantiate_calculator(self):
-        calculator = Calculator()
-        self.assertIsInstance(calculator, Calculator)
+        self.assertIsInstance(self.calculator, Calculator)
 
     def test_results_property_calculator(self):
-        calculator = Calculator()
-        self.assertEquals(calculator.result, 0)
+        self.assertEquals(self.calculator.result, 0)
 
     def test_add_method_calculator(self):
-        calculator = Calculator()
-        self.assertEqual(calculator.add(2, 2), 4)
-        self.assertEqual(calculator.result, 4)
+        self.assertEqual(self.calculator.add(2, 2), 4)
+        self.assertEqual(self.calculator.result, 4)
 
     def test_subtract_method_calculator(self):
-        calculator = Calculator()
-        self.assertEqual(calculator.subtract(2, 2), 0)
-        self.assertEqual(calculator.result, 0)
+        self.assertEqual(self.calculator.subtract(2, 2), 0)
+        self.assertEqual(self.calculator.result, 0)
 
 
 if __name__ != '__main__':
