7008d3c469b188e608f0e565610e006237b3ff6b
HUBRILITY <85511064+HUBRILITY@users.noreply.github.com>
2021-07-14 19:31:04 -0400

    Feature: Updated Mean.py and created SampleMean.py

--- src/Calculator/Statistics/SampleMean.py
+++ src/Calculator/Statistics/SampleMean.py
@@ -2,7 +2,7 @@ from Calculator.Addition import addition
 from Calculator.Division import division
 
 
-def mean(data):
+def sample_mean(data, sample_size):
     num_values = len(data)
     total = 0
     for num in data:
--- src/Calculator/Statistics/Statistics.py
+++ src/Calculator/Statistics/Statistics.py
@@ -1,8 +1,36 @@
 from Calculator.Calculator import Calculator
+from Statistics.Mean import mean
+from Statistics.SampleMean import sample_mean
+from CsvReader.CsvReader import CsvReader
 
 
 class Statistics(Calculator):
     data = []
 
-    def __init__(self):
-        super(Statistics, self).__init__()
+    def __init__(self, filepath):
+        self.data = CsvReader(filepath)
+        super().__init__()
+
+    def mean(self):
+        self.result = mean(self.datab)
+        return self.result
+
+    def sample_mean(self, sample_size):
+        self.result = sample_mean(self.data, sample_size)
+        return self.result
+
+    def multiplication(self, a, b):
+        self.result = multiplication(a, b)
+        return self.result
+
+    def division(self, a, b):
+        self.result = division(a, b)
+        return self.result
+
+    def square(self, a) -> object:
+        self.result = square(a)
+        return self.result
+
+    def squareRoot(self, a):
+        self.result = squareRoot(a)
+        return self.result
