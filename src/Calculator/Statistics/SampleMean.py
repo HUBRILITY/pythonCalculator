1307bdbc9a777ec5146f440c697ed427a8cc481f
HUBRILITY <85511064+HUBRILITY@users.noreply.github.com>
2021-07-14 20:24:46 -0400

    Feature: Created GetSample.py

--- src/Calculator/Statistics/SampleMean.py
+++ src/Calculator/Statistics/SampleMean.py
@@ -5,6 +5,7 @@ from Calculator.Division import division
 def sample_mean(data, sample_size):
     num_values = len(data)
     total = 0
-    for num in data:
+    random_values = random.choice(data, k=sample_size-1))
+    for num in random_values:
         total = addition(total, num)
     return division(total, num_values)
