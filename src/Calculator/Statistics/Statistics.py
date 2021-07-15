2564a1d23b15bca038935875e3cdb0d076d354fc
HUBRILITY <85511064+HUBRILITY@users.noreply.github.com>
2021-07-14 21:08:27 -0400

    Task: Updated Statistics.py and .travis.yml

--- src/Calculator/Statistics/Statistics.py
+++ src/Calculator/Statistics/Statistics.py
@@ -1,6 +1,6 @@
 from Calculator.Calculator import Calculator
-from Statistics.Mean import mean
-from Statistics.SampleMean import sample_mean
+from Calculator.Statistics.Mean import mean
+from Calculator.Statistics.SampleMean import sample_mean
 from CsvReader.CsvReader import CsvReader
 
 
@@ -12,25 +12,9 @@ class Statistics(Calculator):
         super().__init__()
 
     def mean(self):
-        self.result = mean(self.datab)
+        self.result = mean(self.data)
         return self.result
 
     def sampleMean(self, sample_size):
         self.result = sample_mean(self.data, sample_size)
-        return self.result
-
-    def multiplication(self, a, b):
-        self.result = multiplication(a, b)
-        return self.result
-
-    def division(self, a, b):
-        self.result = division(a, b)
-        return self.result
-
-    def square(self, a) -> object:
-        self.result = square(a)
-        return self.result
-
-    def squareRoot(self, a):
-        self.result = squareRoot(a)
         return self.result
